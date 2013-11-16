1052d0e38d5dba290561dc5a28c73c36066dcdcb
Siyao <siyao1030@yahoo.com.cn>
2013-11-15 20:13:27 -0800

    creation

---
+++ LookCast/LookCastTests/LookCastTests.m
@@ -0,0 +1,34 @@
+//
+//  LookCastTests.m
+//  LookCastTests
+//
+//  Created by Siyao Clara Xie on 11/15/13.
+//  Copyright (c) 2013 Siyao Xie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LookCastTests : XCTestCase
+
+@end
+
+@implementation LookCastTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
